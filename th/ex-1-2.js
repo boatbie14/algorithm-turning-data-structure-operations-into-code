b3aae424550b5a6c598c378e259f510700ce86c7
Kittiphat <kittiphat.rpp@gmail.com>
2025-03-06 11:19:34 +0700

    done exercise 1-3

--- th/ex-1-2.js
+++ th/ex-1-2.js
@@ -8,8 +8,15 @@
         - หารด้วย 11 ไม่ลงตัว
     - เมื่อ Algorithm นี้ทำงาน จะต้องเห็นผลลัพธ์ด้านล่าง
 */
+const selectNumber = (number) => {
+  return number % 2 !== 0 && (number % 3 === 0 || number % 5 === 0) && number % 11 !== 0;
+};
 
-// Start coding here
+const sumOddNumber = (numbers) => {
+  let newArray = numbers.filter(selectNumber);
+  console.log(newArray);
+  return newArray.reduce((acc, cur) => acc + cur, 0);
+};
 
 const numbers1 = [1, 2, 3, 5, 6, 8, 11, 13];
 console.log(sumOddNumber(numbers1)); // 8 จาก [3, 5]
